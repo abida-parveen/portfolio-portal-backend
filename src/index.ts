1640c32c612340eda1ef6c9534618b3d226d25ed
Abida Parveen Abro <57529906+abida-parveen@users.noreply.github.com>
2025-07-08 04:43:31 +0500

    feature adding cors

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import authRoutes from "./routes/authRoutes";
 
 // Load environment variables from .env file
@@ -9,6 +10,14 @@ const app = express();
 
 // Read port from the environment variable or default to 8080
 const port = process.env.PORT || 8080;
+
+const allowedOrigin = process.env.FRONTEND_URL || "http://localhost:3000";
+
+app.use(
+  cors({
+    origin: allowedOrigin,
+  })
+);
 
 // Middleware to handle JSON requests
 app.use(express.json());
